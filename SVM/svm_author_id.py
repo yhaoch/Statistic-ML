d7484824d3c4ce9f84f646acfe8712f09fb2bc3a
Haochuan Yang <u6001301@anu.edu.au>
2017-07-20 16:47:33 +1000

    SVM Little Project

---
+++ SVM/svm_author_id.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+#########################################################
+### your code goes here ###
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+clf = svm.SVC(kernel = 'rbf', C = 10000)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+count = 0;
+
+for item in pred:
+	if item == 1:
+		count += 1
+print(count)
+# print(pred[26])
+# print(pred[50])
+
+#print(accuracy_score(labels_test,pred))
+#########################################################
+
+
